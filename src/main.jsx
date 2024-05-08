7d39882a4eaecb15bfc522c3c6a74aa2215af936
DmytroRev <149614107+DmytroRev@users.noreply.github.com>
2024-05-08 18:54:46 +0300

    home

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
-// src/main.jsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./components/App/App";
+import App from "./components/App/App";
+// import { LoginForm } from "./components/Product/Product";
+// import { MyComponent } from "./components/Product/Product";
+// import { SearchBar, LangSwitcher, CheckedForm, HardForm } from "./components/Product/Product";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    {/* <LoginForm />
+    <MyComponent />
+    <SearchBar />
+    <LangSwitcher />
+    <CheckedForm />
+    <HardForm/> */}
   </React.StrictMode>
 );
 
